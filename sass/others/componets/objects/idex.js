b400b607bf714a4da1402c4fac7700cb9345a7ae
Luiz Henrique <120956095+LuizSilva28@users.noreply.github.com>
2024-08-03 18:46:47 -0300

    add objects of standard card

---
+++ sass/others/componets/objects/idex.js
@@ -0,0 +1,102 @@
+const standardcard = [
+	{
+		id: 1,
+		image: "https://via.placeholder.com/150",
+		title: "CSS3 e CSS Moderno",
+		description: {
+			xp: "1 ano",
+			nivel: "75%",
+			projects: 5,
+		},
+	},
+	{
+		id: 2,
+		image: "https://via.placeholder.com/150",
+		title: "JavaScript ",
+		description: {
+			xp: "1 ano",
+			nivel: "75%",
+			projects: 5,
+		},
+	},
+	{
+		id: 3,
+		image: "https://via.placeholder.com/150",
+		title: "ReactJS ",
+		description: {
+			xp: "1 ano",
+			nivel: "75%",
+			projects: 5,
+		},
+	},
+	{
+		id: 4,
+		image: "https://via.placeholder.com/150",
+		title: "NodeJS ",
+		description: {
+			xp: "1 ano",
+			nivel: "75%",
+			projects: 5,
+		},
+	},
+	{
+		id: 5,
+		image: "https://via.placeholder.com/150",
+		title: "Sass",
+		description: {
+			xp: "1 ano",
+			nivel: "75%",
+			projects: 5,
+		},
+	},
+	{
+		id: 6,
+		image: "https://via.placeholder.com/150",
+		title: "Html5 ",
+		description: {
+			xp: "1 ano",
+			nivel: "75%",
+			projects: 5,
+		},
+	},
+	{
+		id: 7,
+		image: "https://via.placeholder.com/150",
+		title: "Bootstrap ",
+		description: {
+			xp: "1 ano",
+			nivel: "75%",
+			projects: 5,
+		},
+	},
+	{
+		id: 8,
+		image: "https://via.placeholder.com/150",
+		title: "Express.js ",
+		description: {
+			xp: "1 ano",
+			nivel: "75%",
+			projects: 5,
+		},
+	},
+	{
+		id: 9,
+		image: "https://via.placeholder.com/150",
+		title: "Kanban",
+		description: {
+			xp: "1 ano",
+			nivel: "75%",
+			projects: 5,
+		},
+	},
+	{
+		id: 10,
+		image: "https://via.placeholder.com/150",
+		title: "PostgreSQL ",
+		description: {
+			xp: "1 ano",
+			nivel: "75%",
+			projects: 5,
+		},
+	},
+];
