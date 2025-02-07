2b1716fbf9208aff1036950219939d7c72fda409
Luiz Henrique <120956095+LuizSilva28@users.noreply.github.com>
2025-02-07 19:14:18 -0300

    corrigindo erro de caminho para fiunção de criação de carrossel

--- sass/index.js
+++ sass/index.js
@@ -1,12 +1,12 @@
 import { allBntsMenu } from "./others/componets/menuMobile/menu/index.js";
 
-import { createCarousel } from "./sass/others/componets/carousel/container/script.js";
+import { createCarousel } from "./others/componets/carousel/container/script.js";
 
 import { createAudio } from "./others/componets/audio/index.js";
 
-import { createGridSkills} from "./others/componets/Grid/index.js"
+import { createGridSkills } from "./others/componets/Grid/index.js";
 
-import {standardCardHardskills} from "./others/componets/objects/hardskills/index.js"
+import { standardCardHardskills } from "./others/componets/objects/hardskills/index.js";
 allBntsMenu();
 
 createCarousel();
