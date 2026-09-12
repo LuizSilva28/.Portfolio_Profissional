bcf65f5e8bf290aa2c23c4163a86a87fa845bc04
Luiz Silva <luizssh54@gmail.com>
2026-09-12 16:40:18 -0300

    firstSection- Delete scripts dulicated

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const getController = require("./controllers/getControllers.js");
-
-const router = express.Router();
-
-router.get("/", getController.indexFrellancers);
-//router.get("/consultsrh", getController.indexConsultsRH);
-
-module.exports = router;
